bb51140b2d6a3b66ffc21f241987bde37e87eeea
young <young@qq.com>
2023-08-20 18:08:56 +0800

    init project

---
+++ test/testGetAddrInfo.cpp
@@ -0,0 +1,49 @@
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    struct addrinfo hints;
+    struct addrinfo *result, *rp;
+
+    if (argc != 3)
+    {
+        std::cerr << "Usage: " << argv[0] << " ip port" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family    = AF_UNSPEC; /* Allow IPv4 or IPv6 */
+    hints.ai_socktype  = SOCK_STREAM; /* Datagram socket */
+    hints.ai_flags     = AI_PASSIVE; /* For wildcard IP address */
+    hints.ai_protocol  = 0; /* Any protocol */
+    hints.ai_canonname = NULL;
+    hints.ai_addr      = NULL;
+    hints.ai_next      = NULL;
+
+    int s = getaddrinfo(argv[1], argv[2], &hints, &result);
+    if (s != 0)
+    {
+        std::cerr << "getaddrinfo: " << gai_strerror(s) << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    /* getaddrinfo() returns a list of address structures.
+       Try each address until we successfully bind(2).
+       If socket(2) (or bind(2)) fails, we (close the socket
+       and) try the next address. */
+
+    for (rp = result; rp != NULL; rp = rp->ai_next) {
+        std::cout << "addrinfo ai_flags " << rp->ai_flags << ", "
+                  << " ai_family " << rp->ai_family << ", "
+                  << " ai_socktype " << rp->ai_socktype << ", "
+                  << " ai_protocol " << rp->ai_protocol << "."
+                  << std::endl;
+    }
+
+    freeaddrinfo(result); /* No longer needed */
+    return 0;
+}
